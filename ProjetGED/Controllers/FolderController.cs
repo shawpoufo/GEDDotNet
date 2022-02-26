a646f36fe57bcdd3989fee1a71170b23e583c5be
shawpoufo <azizihatim11@gmail.com>
2022-02-26 21:52:51 +0100

    idk

--- ProjetGED/Controllers/FolderController.cs
+++ ProjetGED/Controllers/FolderController.cs
@@ -13,18 +13,18 @@ namespace ProjetGED.Controllers
     {
         public ActionResult Upload()
         {
-            TempData["msgUpFolder"] = (isValid:true,message:"");
+            TempData["msgUpFolder"] = (isValid: true, message: "");
             return View();
         }
-        
+
         [HttpPost]
         [Route("Folder/Upload")]
-        public ActionResult Upload(IEnumerable<HttpPostedFileBase> uploadeFolder,string currentFolderPath)
+        public ActionResult Upload(IEnumerable<HttpPostedFileBase> uploadeFolder, string currentFolderPath)
         {
             //crée le dossier s'il n"xiste pas
             //path = userId + currentFolderPath + (extraire nom du dossier)
             //puis crée les document directement sans vérifier car le fichier est nouveau
-            if(uploadeFolder.Count() > 0 && uploadeFolder.First() != null)
+            if (uploadeFolder.Count() > 0 && uploadeFolder.First() != null)
             {
                 try
                 {
@@ -34,60 +34,70 @@ namespace ProjetGED.Controllers
                         string folderName = uploadeFolder.First().FileName.Split('/')[0].Trim();
                         int userId = this.UserId();
                         var user = context.OurUsers.Include("Folders").Where(u => u.Id == userId).First();
+                        context.Entry(user).Collection(u => u.FolderPrivileges).Load();
                         string newFolderPath = Path.Combine(currentFolderPath, folderName);
-                        // vérifier que le fichier qu'on veut ajouter n'existe pas 
-                        // dans la base de donnée de l'utilisateur afin de le crée
-                        // ici on vérifier soulement si l'utilisteur possede déja le fichier mais pas s'il a le privilege
-                        // on doit ajouter la vérification du privilege ici (NotImplemented yet)
-                        // par ce que l'utilisateur ne doit pas charger un dossier qui a le même chemin d'un dossier 
-                        // on doit vérifier que dans le fichier dont on a access ne contient pas un dossier avec le meme nom/chemin
-                        if (!user.Folders.ToList().Exists(f => f.ComparePath(newFolderPath)))
-                        {
-                            
-                            var newFolder = new Folder { Name = folderName, Path = newFolderPath, CreatedAt = DateTime.Now };
-                            
-                            newFolder.Parent = user.Folders.Where(f => f.ComparePath(currentFolderPath)).First();
-                           
-                            
-                            foreach (var document in uploadeFolder)
-                            {
-                                string name = Path.GetFileName(document.FileName);
+                        Folder folderParrent = context.Folders.ToList().FirstOrDefault(f => f.ComparePath(currentFolderPath));
 
-                                newFolder.Documents.Add(new Document
-                                {
-                                    Name = name,
-                                    Author = user,
-                                    Path = Path.Combine(newFolder.Path, name),
-                                    UploadedAt = DateTime.Now,
-                                    Version = 0
-                                });
-                            }
-                            user.Folders.Add(newFolder);
-                            if(context.SaveChanges() > 0)
+                        if (folderParrent != null)
+                        {
+                            bool check = false;
+                            if (user.Folders.ToList().Exists(f => f.Id == folderParrent.Id))
+                                check = true;
+                            else if (!user.FolderPrivileges.ToList().Exists(f => f.FolderId == folderParrent.Id))
                             {
-                                string physicalFolderPath = Path.Combine(Server.MapPath("~/cloud"), newFolder.Path);
-                                Directory.CreateDirectory(physicalFolderPath);
-                                foreach(var document in uploadeFolder)
+                                return new HttpUnauthorizedResult("L'accés à ce Dossier est non autorisé");
+                            }
+                            else if (!context.FolderPrivileges.Find(user.Id, folderParrent.Id).Write)
+                            {
+                                TempData["msgUpFolder"] = (isValid: false, message: "Vous n'avez pas le droit de charger des documents dans ce dossier");
+                            }
+                            else
+                            {
+                                check = true;
+                            }
+
+                            if (check)
+                            {
+                                var newFolder = new Folder { Name = folderName, Path = newFolderPath, CreatedAt = DateTime.Now };
+                                newFolder.Parent = folderParrent;
+                                foreach (var document in uploadeFolder)
                                 {
-                                    document.SaveAs(Path.Combine(physicalFolderPath, Path.GetFileName(document.FileName)));
+                                    string name = Path.GetFileName(document.FileName);
+
+                                    newFolder.Documents.Add(new Document
+                                    {
+                                        Name = name,
+                                        Author = user,
+                                        Path = Path.Combine(newFolder.Path, name),
+                                        UploadedAt = DateTime.Now,
+                                        Version = 0
+                                    });
                                 }
-                                TempData["msgUpFolder"] = (isValid:true,message:"le Dossier est sauvegarder avec success");
+                                context.Folders.Add(newFolder);
+
+                                if (context.SaveChanges() > 0)
+                                {
+                                    string physicalFolderPath = Path.Combine(Server.MapPath("~/cloud"), newFolder.Path);
+                                    Directory.CreateDirectory(physicalFolderPath);
+                                    foreach (var document in uploadeFolder)
+                                    {
+                                        document.SaveAs(Path.Combine(physicalFolderPath, Path.GetFileName(document.FileName)));
+                                    }
+                                    TempData["msgUpFolder"] = (isValid: true, message: "le Dossier est sauvegarder avec success");
+                                }
                             }
                         }
                         else
-                            TempData["msgUpFolder"] = (isValid:false,message:"le Dossier existe déja");
-
+                            return HttpNotFound("Dossier inexistant");
                     }
-                    
                 }
-                catch 
+                catch(Exception ex)
                 {
-
                     TempData["msgUpFolder"] = (isValid: false, message: "le Dossier n'a pas était charger");
                 }
             }
             else
-                TempData["msgUpFolder"] = (isValid:false,message:"aucun dossier selectionner !");
+                TempData["msgUpFolder"] = (isValid: false, message: "Aucun dossier selectionner !");
 
             return RedirectToAction("Index", new { slug = currentFolderPath });
         }
@@ -96,29 +106,38 @@ namespace ProjetGED.Controllers
         [Route("Folder/{*slug}")]
         public ActionResult Index(string slug)
         {
-           
+
             using (var context = new GEDContext())
             {
                 var id = this.UserId();
                 var user = context.OurUsers.Include("Folders").First(u => u.Id == id);
-                Folder folder = null;
+                context.Entry(user).Collection(u => u.FolderPrivileges).Load();
                 if (string.IsNullOrEmpty(slug))
                     return new HttpNotFoundResult("Ce dossier n'existe pas");
-                    //folder = user.Folders.First(f => f.Name == user.Id.ToString());
                 else
                 {
-                    if (user.Folders.ToList().Exists(f => f.ComparePath(slug)))
-                        folder = user.Folders.Where(f => f.ComparePath(slug)).First();
-                    // ici on doit vérifier s'il existe dans la table privilege
-                    else
+                    Folder folder = context.Folders.ToList().FirstOrDefault(f => f.ComparePath(slug));
+                    if (user.Folders.ToList().Exists(f => f.ComparePath(slug)) || user.FolderPrivileges.ToList().Exists(f => f.Folder.ComparePath(slug)))
+                    {
+                        context.Entry(folder).Collection(r => r.Documents).Load();
+                        ViewData["folders"] = user.Folders.ToList().FindAll(f => f.Parent == folder);
+                        ViewData["documents"] = folder.Documents;
+                    }
+                    else if (folder != null && !user.FolderPrivileges.ToList().Exists(f => f.Folder.ComparePath(slug)))
+                    {
+                        // crée une rdirection customized
+                        return Content(System.Net.HttpStatusCode.Unauthorized.ToString(), "Not authorized");
+                    }
+                    else 
                         return new HttpNotFoundResult("Ce dossier n'existe pas");
-
+                   
                 }
-                context.Entry(folder).Collection(r => r.Documents).Load();
-                ViewData["folders"] = user.Folders.ToList().FindAll(f => f.Parent == folder);
-                ViewData["documents"] = folder.Documents;
+                
             }
             return View();
         }
+
+        
     }
+    
 }
